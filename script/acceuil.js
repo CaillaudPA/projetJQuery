8df869ee113f332f63aba8a4ca4654ff326d15bd
coco35700 <jezequel35@gmail.com>
2015-04-03 14:25:13 +0200

    histoired'yvoir

--- script/acceuil.js
+++ script/acceuil.js
@@ -4,42 +4,54 @@ $(document).ready(function(){
 		getPhoto();
 	});
 
+    $('.rowPhoto').slick({  
+        dots: true,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear'
+    });
+
 	$( "#ville" ).autocomplete({
-source: function(requete,reponse){
-    $.ajax({
-        url : "​http://annuaire.comarquage.fr/rest/autocomplete_territory.json?",
-        data : "term=nantes",
-        dataType : "json",
-        method : "GET",
-    success : function (json) {
-        Villes = [];
-            $.each(json, function(i,item){
-                //supprimer les doublons
-                if ($.inArray(item.Ville,Villes) == -1){
-                Villes.push(item.Ville);
-                //$('#submit').css({"visibility" :'visible'});
-                //$('#reset').css({"visibility" :'visible'});
-            }
-        });
-                reponse(Villes);
-            },
-                error : function (){
-                reponse(["rien"]);
-            }
-        });
-    }
-});
-	
+        source: function(requete,reponse){
+            $.ajax({
+                url : "​http://infoweb-ens/~jacquin-c/codePostalComplete.php",
+                data : "maxRows=10&commune=" + requete.term,
+                dataType : "json",
+                method : "GET",
+                success : function (json) {
+                    Villes = [];
+                    $.each(json, function(i,item){
+                        //supprimer les doublons
+                        if ($.inArray(item.Ville,Villes) == -1){
+                        Villes.push(item.Ville);
+                        //$('#submit').css({"visibility" :'visible'});
+                        //$('#reset').css({"visibility" :'visible'});
+                        }
+                    });
+                    reponse(Villes);
+                },
+                    error : function (){
+                    reponse(["rien"]);
+                    }
+            });
+        }
+    });	
 });
 
 
 function getPhoto (){
-	$('#rowPhoto').empty();
+	$('#row').empty();
 	$.getJSON("http://api.flickr.com/services/feeds/photos_public.gne?tags="+ $('#ville').val() +"&tagmode=any&format=json&jsoncallback=?",
 	function(data){
 		$.each(data.items, function(i,item){
 			//$("<div id='divPhoto' class='col-xs-4 col-sm-3 col-md-2'></div>").appendTo("#rowPhoto");
-			$("<img id='tailleImg'/>").attr("src", item.media.m).appendTo("#rowPhoto");
+			$("<img />").attr("src", item.media.m).appendTo("#row").wrap("<div></div>");
+            if(i==4){
+                return false;
+            }
 		});
 	});
 }
+
+
