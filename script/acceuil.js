db07088ea1c69412d660417aa6bfe3d40d35bbd3
CaillaudPA <caillaud.pierreantoine@yahoo.com>
2015-04-05 11:35:51 +0200

    ajout html + js

--- script/acceuil.js
+++ script/acceuil.js
@@ -39,21 +39,25 @@ function getPhoto (){
 	$("#rowPhoto").empty();
     $("#rowPhoto").html('<div id="owl-demo" class="owl-carousel"></div>');
 
-	$.getJSON("http://api.flickr.com/services/feeds/photos_public.gne?tags="+ $('#ville').val() +"&tagmode=any&format=json&jsoncallback=?",
-	function(data){
-       
-		$.each(data.items, function(i,item){
-			//$("<div id='divPhoto' class='col-xs-4 col-sm-3 col-md-2'></div>").appendTo("#rowPhoto");
-
-			content += "<div class='item'> <img src='"+ item.media.m+"' /></div>"; //;$("<img id='tailleImg'/>").attr("src", item.media.m).appendTo("#rowPhoto");
-		     $('#owl-demo').html(content);
-             setTimeout(function(){$('#owl-demo').owlCarousel({navigation : true, // Show next and prev buttons
-              slideSpeed : 300,
-              paginationSpeed : 400,
-              singleItem:true});},2000);
     
-        });
-	});
+
+    	$.getJSON("http://api.flickr.com/services/feeds/photos_public.gne?tags="+ $('#ville').val() +"&tagmode=any&format=json&jsoncallback=?",
+    	function(data){
+           
+    		$.each(data.items, function(i,item){
+    			//$("<div id='divPhoto' class='col-xs-4 col-sm-3 col-md-2'></div>").appendTo("#rowPhoto");
+                    if ($('#checkBoxCcarousel').is(':checked')){
+        			content += "<div class='item'> <img src='"+ item.media.m+"' /></div>"; //;$("<img id='tailleImg'/>").attr("src", item.media.m).appendTo("#rowPhoto");
+        		    $('#owl-demo').html(content);
+                    setTimeout(function(){$('#owl-demo').owlCarousel({navigation : true, // Show next and prev buttons
+                    slideSpeed : 300,
+                    paginationSpeed : 400,
+                    singleItem:true});},2000);
+                }
+        
+            });
+    	});
+
 
     
 }
