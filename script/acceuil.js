d4552afcbc8ca9da150c752b52509df4d3560f08
coco35700 <jezequel35@gmail.com>
2015-04-06 22:58:23 +0200

    vPropre

--- script/acceuil.js
+++ script/acceuil.js
@@ -1,35 +1,31 @@
 $(document).ready(function(){
-   
-    ville = $('#ville').val();
     // de base la vue est en carousel
     var affichage ="liste";
-    var select = $("#nombre");
+    // initialisation du tableau qui contiendra nos images sous forme d'objet
     var images = []
 
-
+    // Au changement de ville on charge les nouvelles photos
 	$("#ville").change(function(){
-        // on rempli le dictionnaire
         var ville = $('#ville').val();
-        //var nombre = select.options[select.selectedIndex].text;
-        var nombre = select.val();
         getPhoto(ville);
         loads();
 	});
 
-
+    // A la demande de tri, on tri le tableau et on re-affiche le tout suivant l'affichage demandé 
     $("#sort").on("click",function(){
-        images = sortByAuthor(images);
+        sortByAuthor();
         changerVue(images, affichage);
         loads();
     });
 
+    // Quand on demande l'affichage en liste, le changement se fait ( voir changerVue )
     $("#list").on("click",function(){
-
         affichage = "liste";
         changerVue(images, affichage);
         loads();
     });
 
+    // Quand on demande l'affichage en Carousel, le changement se fait ( voir changerVue )
     $("#carousel").on("click",function(){
         affichage = "carousel";
         changerVue(images, affichage);
@@ -37,59 +33,62 @@ $(document).ready(function(){
     });
 
 
+     // mise en place de l'autocomplete
+    $( "#ville" ).autocomplete({
+        source: function(requete,reponse){
+            // mise en place de la requette ajax
+            $.ajax({
+                url : "http://infoweb/~jacquin-c/codePostalComplete.php",
+                data : "maxRows=10&commune=" + requete.term,
+                dataType : "json",
+                method : "GET",
+
+                success : function (json) {
+                Villes = [];
+                    $.each(json, function(i,item){
+                        //supprimer les doublons
+                        if ($.inArray(item.Ville,Villes) == -1){
+                        Villes.push(item.Ville);
+                        $('#submit').css({"visibility" :'visible'});
+                        $('#reset').css({"visibility" :'visible'});
+                        }
+                    });
+                        reponse(Villes);
+                    },
+                        error : function (){
+                        reponse([]);
+                    }
+            });
+        }
+    });
+
+    // cette fonction permet de charger les images pour la fenetre modale
     function loads(){
+        /* pour chaque image dans le body on la compare avec une image dans notre tableau 
+           si elles correspondes, on selectionne l'indice du tableau et on le renvoi pour la fenetre modale
+        */
         $("body").find("img").on("click",function(){
             var indice = 0;
-             for(var i = 0 ; i < $('#nombre').val() ; i++){
-            if(images[i].url == $(this).attr("src")){
-                indice = i;
+            for(var i = 0 ; i < $('#nombre').val() ; i++){
+                if(images[i].url == $(this).attr("src")){
+                    indice = i;
+                }
             }
-        }
             clicks(indice);
         });
     }
 
-
-     // mise en place de l'autocomplete
-        $( "#ville" ).autocomplete({
-            source: function(requete,reponse){
-                $.ajax({
-                    url : "http://infoweb/~jacquin-c/codePostalComplete.php",
-                    data : "maxRows=10&commune=" + requete.term,
-                    dataType : "json",
-                    method : "GET",
-
-                    success : function (json) {
-                    Villes = [];
-                        $.each(json, function(i,item){
-                            //supprimer les doublons
-                            if ($.inArray(item.Ville,Villes) == -1){
-                            Villes.push(item.Ville);
-                            $('#submit').css({"visibility" :'visible'});
-                            $('#reset').css({"visibility" :'visible'});
-                            }
-                        });
-                            reponse(Villes);
-                        },
-                            error : function (){
-                            reponse([]);
-                        }
-                });
-            }
-        });
-
+    // ce fonction permet grace a loads défini précédament de remlir la fenetre modale avec les informations de l'image
     function clicks(indice){
-        
-        //alert(indice);
+
         $('.modal').html(" <span class='modal_close'>&#215;</span>" + images[indice].titleImg +" <br/> "+ images[indice].date +" <br/> " + images[indice].auteur +" <br/> "+ images[indice].url);
         $('.modal').css('display', 'block');
         $('.modal_close').click(function(){
         $('.modal').css('display', 'none');
          });
-        //window.alert(images[i].titleImg +"  "+ images[i].date +" | " + images[i].auteur +" | "+ images[i].url);  
     }
 
-
+    // rempli le tableau d'objets images grace a une requette getJSON
     function getPhoto (ville){
         images = [];
         $.getJSON("http://api.flickr.com/services/feeds/photos_public.gne?tags="+ ville +"&tagmode=any&format=json&jsoncallback=?",
@@ -107,12 +106,12 @@ $(document).ready(function(){
         });
     }
 
-    function sortByAuthor(dico){
-        var newTab = [];
-        newTab = dico.sort(compareAuthor);
-        return newTab;
+    // tri notre tableau d'images grace a notre comparateur personnalisé
+    function sortByAuthor(){
+        images = images.sort(compareAuthor);
     }
 
+    // notre comparateur, tri par rapport aux auteurs
     function compareAuthor(a,b) {
         if (a.auteur < b.auteur)
             return -1;
@@ -121,32 +120,34 @@ $(document).ready(function(){
         return 0;
     }
 
-       
-
-    function changerVue(dico,vue){
+    // permet d'effectuer le changement de vue entre carousel et liste
+    function changerVue(vue){
+        // si la vue est carousel on vide l'espace et on rempli avec nos photos 
+        // contenues dans la liste ( en suivant le modele de carousel )
         if(vue == "carousel"){
+
             $("#rowPhoto").empty();
             $("#rowPhoto").html('<div id="owl-demo" class="owl-carousel"></div>');
 
             var content = "";
 
-                for (var i = 0; i < $('#nombre').val(); i++) {
-                    content += "<div class='item'> <img class='imgListe' src='"+ dico[i].url +"' /></div>"; //;$("<img id='tailleImg'/>").attr("src", item.media.m).appendTo("#rowPhoto");
-                    $('#owl-demo').html(content);
-                    setTimeout(function(){$('#owl-demo').owlCarousel({navigation : true, // Show next and prev buttons
-                    slideSpeed : 300,
-                    paginationSpeed : 400,
-                    singleItem:true});},100);
-                };
-            }
-            else{
+            for (var i = 0; i < $('#nombre').val(); i++) {
+                content += "<div class='item'> <img class='imgListe' src='"+ images[i].url +"' /></div>"; //;$("<img id='tailleImg'/>").attr("src", item.media.m).appendTo("#rowPhoto");
+                $('#owl-demo').html(content);
+                setTimeout(function(){$('#owl-demo').owlCarousel({navigation : true, // Show next and prev buttons
+                slideSpeed : 300,
+                paginationSpeed : 400,
+                singleItem:true});},100);
+            };
+        }
+        // sinon c'est la demande de liste
+        // pareil on vide l'espace et on rempli en suivant notre formattage en liste
+        else{
 
             $("#rowPhoto").empty();
-            //$('#rowPhoto').html("<div class='row' id='row'></div>");
-
             for (var i = 0; i< $('#nombre').val(); i++) {
-                var titre = dico[i].titleImg;
-                $("#rowPhoto").append("<div class='col-lg-3 col-md-4 col-sm-6 col-xs-12'><img id='imgListe' src='"+dico[i].url+"'/></div>");
+                var titre = images[i].titleImg;
+                $("#rowPhoto").append("<div class='col-lg-3 col-md-4 col-sm-6 col-xs-12'><img id='imgListe' src='"+images[i].url+"'/></div>");
             };
         }
     }
